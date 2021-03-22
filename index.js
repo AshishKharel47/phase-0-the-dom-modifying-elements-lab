77af6a85aeeb65d3878b56e2eb740ddbf3ea4510
AshishKharel <ashishkharel58@gmail.com>
2021-03-21 20:55:30 -0400

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main').remove();
+const newHeader=document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id="victory";
+document.getElementById("victory").innerHTML='Ashish is the champion';
